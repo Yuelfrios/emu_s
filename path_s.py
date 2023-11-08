064c944839841e63f5a123f7640e545abc872679
yuel_frios <nlara@andreani.com>
2023-11-08 12:54:23 -0300

    se dividio el tiempo de tinme.sleep(.5)

---
+++ path_s.py
@@ -0,0 +1,17 @@
+from keyboard import KEY_DOWN, hook, wait
+from pyautogui import press
+from time import sleep
+barcode_data = ""
+target_sequence = "123456"
+
+def on_key_event(e):
+    global barcode_data
+    if e.event_type == KEY_DOWN:
+        barcode_data += e.name
+        if barcode_data.endswith(target_sequence):
+            sleep(0.5)
+            press('s')
+
+hook(on_key_event)
+
+wait('esc')
